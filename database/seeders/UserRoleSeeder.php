388bfcaf12963e5940189266cd46d9758e5c7654
bangsile <asilt94@gmail.com>
2025-09-14 12:10:37 +0900

    init project

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\PermissionRegistrar;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        $this->role();
+        $this->user();
+    }
+
+    public function user()
+    {
+        $user = \App\Models\User::create([
+            'name' => "superadmin",
+            'email' => "superadmin@mail.com",
+            'password' => Hash::make('a'),
+        ]);
+        $user->assignRole('superadmin');
+    }
+
+    public function role()
+    {
+        Role::create(['name' => 'superadmin']);
+        Role::create(['name' => 'user']);
+    }
+}
